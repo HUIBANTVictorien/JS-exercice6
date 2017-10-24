e63e01f3a34f424f11d4ff6736e123ecb26910d1
Victorien HUIBANT <huibant.victorien@gmail.com>
2017-10-24 12:26:51 +0200

    changement code

--- exercice6.js
+++ exercice6.js
@@ -1,6 +1,6 @@
-function controle(form) {
-  var first = document.form.premier_nombre.value;
-  var second = document.form.deuxieme_nombre.value;
+function display() {
+  var first = document.getElementById('premier_nombre').value;
+  var second = document.getElementById('deuxieme_nombre').value;
   var premier = Math.trunc(first);
   alert(premier % second);
 };

caa854e498dc47e7c1a175dc63304936e48fa017
Victorien HUIBANT <huibant.victorien@gmail.com>
2017-10-26 09:17:01 +0200

    ajout var anglais

--- exercice6.js
+++ exercice6.js
@@ -1,6 +1,6 @@
 function display() {
   var first = document.getElementById('premier_nombre').value;
   var second = document.getElementById('deuxieme_nombre').value;
-  var premier = Math.trunc(first);
-  alert(premier % second);
+  var one = Math.trunc(first);
+  alert(one % second);
 };
